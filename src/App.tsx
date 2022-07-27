66a9c5e1ebd90d3b9c09b70539bc57add83ea76d
Alejo Viola <viola.alejo@outlook.com>
2022-07-27 18:03:26 -0300

    [App] - Creating color selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
+// REACT
+import { useState } from "react";
+
+// STYLES
+import styles from "./App.module.sass";
+
 // Components
 import Pixel from "./components/Pixel/Pixel";
 
 function App() {
+  const [selectedColor, setSelectedColor] = useState<`#${string}`>("#333333");
+
   return (
     <div className="App">
+      <div>
+        <button
+          className={styles.SelectColor}
+          style={{ background: "#FF8E00" }}
+        ></button>
+        <button
+          className={styles.SelectColor}
+          style={{ background: "#003F7D" }}
+        ></button>
+        <button
+          className={styles.SelectColor}
+          style={{ background: "#333333" }}
+        ></button>
+      </div>
+
       <Pixel />
     </div>
   );
